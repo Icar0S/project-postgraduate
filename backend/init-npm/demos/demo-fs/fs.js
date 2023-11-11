552d8a57d3b0fec78491f9283e17622f089aab6f
icar0s <icarooliveira_23@hotmail.com>
2023-11-11 20:02:45 -0300

    adding folders with demos of modules interactions

---
+++ backend/init-npm/demos/demo-fs/fs.js
@@ -0,0 +1,10 @@
+import { createServer } from 'http';
+import { readFile } from 'fs';
+
+createServer(function (req, res) {
+  readFile('readfile.html', function (err, data) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
